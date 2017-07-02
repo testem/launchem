bb8f9981f165ac78ee419055072828d3d0a0e295
Trent Willis <trentmwillis@gmail.com>
2017-07-02 14:24:23 -0700

    Initial port of code from Testem

---
+++ tests/fileutils_tests.js
@@ -0,0 +1,82 @@
+'use strict';
+
+var path = require('path');
+var childProcess = require('child_process');
+var EventEmitter = require('events').EventEmitter;
+
+var expect = require('chai').expect;
+var sinon = require('sinon');
+
+var fileutils = require('../lib/fileutils');
+var addToPATH = require('../lib/add-to-PATH');
+
+describe('fileutils', function() {
+  var sandbox;
+
+  beforeEach(function() {
+    sandbox = sinon.sandbox.create();
+  });
+
+  afterEach(function() {
+    sandbox.restore();
+  });
+
+  describe('fileExists', function() {
+    it('returns true for an existing file', function() {
+      return fileutils.fileExists(__filename).then(function(result) {
+        expect(result).to.be.true();
+      });
+    });
+
+    it('returns false for an existing directory', function() {
+      return fileutils.fileExists(__dirname).then(function(result) {
+        expect(result).to.be.false();
+      });
+    });
+
+    it('returns false for a not existing directory', function() {
+      return fileutils.fileExists('./not-existing.js').then(function(result) {
+        expect(result).to.be.false();
+      });
+    });
+  });
+
+  describe('executableExists', function() {
+    it('returns true for an existing executable', function() {
+      return fileutils.executableExists('node').then(function(result) {
+        expect(result).to.be.true();
+      });
+    });
+
+    it('allows to define custom options', function() {
+      var options = {
+        env: addToPATH(path.join(process.cwd(), 'tests/fixtures/processes'))
+      };
+
+      return fileutils.executableExists('bin-test', options).then(function(result) {
+        expect(result).to.be.true();
+      });
+    });
+
+    it('returns false for an not existing executable', function() {
+      return fileutils.executableExists('not-found').then(function(result) {
+        expect(result).to.be.false();
+      });
+    });
+
+    it('returns false for not existing executables with a custom which', function() {
+      var process = new EventEmitter();
+
+      sandbox.stub(childProcess, 'spawn', function() {
+        setTimeout(function() {
+          process.emit('close', 127);
+        });
+        return process;
+      });
+
+      return fileutils.executableExists('not-found').then(function(result) {
+        expect(result).to.be.false();
+      });
+    });
+  });
+});
---
+++ tests/fixtures/firefox/custom_user.js
@@ -0,0 +1,7 @@
+/* global user_pref */
+
+'use strict';
+
+user_pref('browser.shell.checkDefaultBrowser', false);
+user_pref('browser.cache.disk.smart_size.first_run', false);
+user_pref('dom.max_script_run_time', 0);
